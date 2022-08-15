f6652e8e3e9a0c551b8ff711b361cb8643a86747
Wricha Kashyap <111267536+wricha28@users.noreply.github.com>
2022-08-15 20:59:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('my parents new healthy dinner')
+streamlit.text('my parents new healthy dinner')
 streamlit.title('Breakfast Menu')
 streamlit.write('omega 3 & blueberry oatmeal')
 streamlit.write('kale,spinach & rocket smoothie')
